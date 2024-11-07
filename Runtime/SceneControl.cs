198b27d07aab7952722f55719c6a60b7fb571747
Fidowen <113884471+Fidowen@users.noreply.github.com>
2024-11-07 11:05:50 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Fido.Tools
+{
+    /// <summary>
+    /// 場景控制 載入場景及離開遊戲
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName">要載入的場景名稱</param>
+        public void LoadScene(string sceneName)
+        {
+            //場景控制器.載入場景(名稱)
+            SceneManager.LoadScene(1);
+        }
+        /// <summary>
+        /// 離開應用程式
+        /// </summary>
+        public void QuitScene()
+        {
+            //應用程式.離開
+            Application.Quit();
+        }
+    }
+}
+
